40aea9b508ecbc211c48332c4dd29c29327ead3d
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2023-12-26 18:40:04 -0300

    fix: :bug: Link para concluir cadastro

--- app/utils.py
+++ app/utils.py
@@ -64,7 +64,7 @@ def send_simple_message(email, token):
                             <p style="font-weight: 900; font-size: 8rem;">be</p>
                             <p style="font-weight: 400; font-size: 4rem;">Alteração de senha</p>
                             <p style="font-weight: 400; font-size: 2rem;">Para alterar a sua senha, clique no botão abaixo. Caso não tenha feito a solicitação, nenhuma ação é necessária.</p>
-                            <a href="http://127.0.0.1/reset-password?{token}&tk={token}" style="margin-top: 75px; text-decoration: none; color: #fff; font-weight: 900; font-size: 2rem; background: #ffcd2e; color: #fff; border-radius: 3px; border-color: #ffcd2e; padding: 20px;">Alterar senha</a>
+                            <a href="http://http://54.193.213.8//reset-password?{token}&tk={token}" style="margin-top: 75px; text-decoration: none; color: #fff; font-weight: 900; font-size: 2rem; background: #ffcd2e; color: #fff; border-radius: 3px; border-color: #ffcd2e; padding: 20px;">Alterar senha</a>
                         </div>
                             
                     </div>
@@ -115,7 +115,7 @@ def send_confirmation(email, token):
                             <p style="font-weight: 900; font-size: 8rem;">be</p>
                             <p style="font-weight: 400; font-size: 4rem;">Bem-vindo</p>
                             <p style="font-weight: 400; font-size: 2rem;">Obrigado por se registrar. Para confirmar sua conta, clique no link abaixo. Caso não tenha feito o cadastro, nenhuma ação é necessária</p>
-                            <a href="http://127.0.0.1/confirm?{token}&tk={token}" style="margin-top: 75px; text-decoration: none; color: #fff; font-weight: 900; font-size: 2rem; background: #ffcd2e; color: #fff; border-radius: 3px; border-color: #ffcd2e; padding: 20px;">Confirmar conta</a>
+                            <a href="http://http://54.193.213.8//confirm?{token}&tk={token}" style="margin-top: 75px; text-decoration: none; color: #fff; font-weight: 900; font-size: 2rem; background: #ffcd2e; color: #fff; border-radius: 3px; border-color: #ffcd2e; padding: 20px;">Confirmar conta</a>
                         </div>
                             
                     </div>
